a7992c22f410d4c5b5fd22ea75805a7d238ca1ae
IceOnFire <antony.mistretta@gmail.com>
2022-05-30 13:16:10 +0200

    Explain env variables and executable scripts

--- 01-data-types/01-numbers.py
+++ 01-data-types/01-numbers.py
@@ -6,7 +6,8 @@
 17 % 3  # remainder operator = 2
 5 * 3 + 2 # floored quotient * divisor + remainder
 2 ** 3 # power
-3 + 5j # complex numbers!
+.1 # you can omit the 0 on floating point numbers
+3 + 5j # Python has built-in complex numbers!
 
 1_000_000 # big numbers can use _ as a separator
 10_00_00_0 # you can place it anywhere
