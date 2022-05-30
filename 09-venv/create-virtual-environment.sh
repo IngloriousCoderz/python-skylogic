969973f7327cb7f0b0b29bb4c6d14dca33cd5bba
IceOnFire <antony.mistretta@gmail.com>
2022-05-30 12:59:33 +0200

    Explain virtual environments

---
+++ 09-venv/create-virtual-environment.sh
@@ -0,0 +1,49 @@
+# Create a virtual environment
+python -m venv .venv
+
+# Check the current paths in Python
+import sys
+sys.path
+# ['', '/usr/lib/python310.zip', '/usr/lib/python3.10', '/usr/lib/python3.10/lib-dynload', '/usr/lib/python3.10/site-packages']
+
+# Activate the virtual environment
+# Windows:
+.venv\Scripts\activate.bat
+# Unix:
+source .venv/bin/activate
+
+# Then check again
+import sys
+sys.path
+# ['', '/usr/lib/python310.zip', '/usr/lib/python3.10', '/usr/lib/python3.10/lib-dynload', '/home/iceonfire/Projects/AxL Formazione/Skylogic/python-skylogic/09-venv/.venv/lib/python3.10/site-packages']
+
+# Installing packages
+
+python -m pip install novas
+# or
+pip install novas==3.1.1.5 # optional version
+
+# Upgrading packages
+
+pip install --upgrade pip
+
+# Try importing novas from global Python, then from within the venv
+import novas
+
+# Show all installed packages
+
+pip list
+
+# Display info about a package
+
+pip show novas
+
+pip freeze > requirements.txt
+
+# Uninstalling packages
+
+pip uninstall novas
+
+# Installing from requirements list
+
+pip install -r requirements.txt
