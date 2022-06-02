b81a927ff663c192b5888268d8776688c3b8389d
IceOnFire <antony.mistretta@gmail.com>
2022-06-02 13:03:27 +0200

    Create more multi-threading examples

--- 08-stdlib/10-multi-threading.py
+++ 08-stdlib/10-multi-threading.py
@@ -3,6 +3,8 @@ import random, time
 
 numbers = range(1, 5)
 
+# Simple multi-threading: just wait for tasks to complete
+
 def long_task(name):
   print(f'Starting task {name}...')
   time.sleep(random.choice(numbers))
@@ -19,11 +21,38 @@ for thread in threads:
   thread.join()
 print('All done.')
 
-# A simpler example taken from https://realpython.com/intro-to-python-threading/
+# Collecting the results is trickier: we need to store results in a list
+
+results = [None for _ in numbers]
+
+def long_task(name):
+  print(f'Starting task {name}...')
+  time.sleep(random.choice(numbers))
+  print(f'Task {name} done.')
+  index = name - 1
+  results[index] = name
+
+threads = [Thread(target=long_task, args=(i,)) for i in numbers]
+
+print('Starting threads...')
+for thread in threads:
+  thread.start()
+print('Waiting for threads to complete...')
+for thread in threads:
+  thread.join()
+print('Tasks', [result for result in results], ': all done.')
+
+# A simpler and better way, taken from https://realpython.com/intro-to-python-threading/
 
 from concurrent.futures import ThreadPoolExecutor
 
+def long_task(name):
+  print(f'Starting task {name}...')
+  time.sleep(random.choice(numbers))
+  print(f'Task {name} done.')
+  return name
+
 print('Starting threads with a pool...')
 with ThreadPoolExecutor(max_workers=4) as executor:
-  executor.map(long_task, numbers)
-print('All done.')
+  results = executor.map(long_task, numbers)
+print('Tasks', [result for result in results], ': all done.')
